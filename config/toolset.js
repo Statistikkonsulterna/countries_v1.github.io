49bac245d792d56815532548141d916b7acf7193
Statistikkonsulterna <magnus.pettersson@statistikkonsulterna.se>
2025-05-26 10:44:30 +0200

    test

--- config/toolset.js
+++ config/toolset.js
@@ -1,19 +1,5 @@
 var toolsPage_toolset = [
   {
-    "id": "bubble_bransch",
-    "tool": "BubbleChart",
-    "toolVariation": "Base",
-    "mainMarker": "bubble",
-    "config": "BubbleChart-basic",
-    "dataSources": ["tva_branscher"],
-    "transition": "none",
-    "title": "Bransch",
-    "hideThumbnail": false,
-    "image": "assets/images/chart/bubble-chart.png",
-    "icon": "assets/images/chart/vizabi_bubbles.svg",
-    "url": ""
-  },
-  {
     "id": "bubble_countries",
     "tool": "BubbleChart",
     "toolVariation": "Base",
@@ -22,20 +8,6 @@ var toolsPage_toolset = [
     "dataSources": ["tva_countries"],
     "transition": "none",
     "title": "Countries",
-    "hideThumbnail": false,
-    "image": "assets/images/chart/bubble-chart.png",
-    "icon": "assets/images/chart/vizabi_bubbles.svg",
-    "url": ""
-  },
-  {
-    "id": "bubble_lan",
-    "tool": "BubbleChart",
-    "toolVariation": "Base",
-    "mainMarker": "bubble",
-    "config": ["BubbleChart-basic"],
-    "dataSources": "tva_lan",
-    "transition": "none",
-    "title": "Län",
     "hideThumbnail": false,
     "image": "assets/images/chart/bubble-chart.png",
     "icon": "assets/images/chart/vizabi_bubbles.svg",
